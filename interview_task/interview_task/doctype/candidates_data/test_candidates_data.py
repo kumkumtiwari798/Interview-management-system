6eafa6b073378b9406df75e0fdf001363730a483
Kumkum Tiwari <kumkumtiwari798@gmail.com>
2024-09-19 20:41:10 +0530

    Initial commit for interview management system

---
+++ interview_task/interview_task/doctype/candidates_data/test_candidates_data.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Kumkum Tiwari and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestCandidatesData(FrappeTestCase):
+	pass
---
+++ interview_task/interview_task/doctype/interviewers/interviewers.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Kumkum Tiwari and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Interviewers(Document):
+	pass
